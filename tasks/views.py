bc33b2a1418b5461e7085123ca06c0c7d0de5d3e
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2023-01-14 11:55:42 -0600

    Agregando login y autentificacion a la aplicacion

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth  import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
@@ -21,9 +21,10 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
             # register user
             try:
-                create_user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                create_user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 create_user.save()
-                login(request, create_user) #Agregar sesion id  
+                login(request, create_user)  # Agregar sesion id
                 return redirect('tasks')
             except:
                 return render(request, 'signup.html',
@@ -38,8 +39,28 @@ def signup(request):
 
 
 def tasks(request):
-  return render(request, 'tasks.html')
+    return render(request, 'tasks.html')
+
 
 def signout(request):
     logout(request)
     return redirect('index  ')
+
+
+def signin(request):
+    if request.method == "GET":
+        return render(request, 'login.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+
+        if user is None:
+            return render(request, 'login.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)  # Para guardar la sesion
+            return redirect('tasks')
