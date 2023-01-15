c016494fdaac4d63a6afc27d2c94a29048524f32
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2023-01-15 08:59:17 -0600

    Agregando el modelo Task a la BD

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
+
+#Para mostrar el campo de fecha de creacion en el panel de administrador
+class TaskAdmin(admin.ModelAdmin):
+  readonly_fields = ("created", )
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
