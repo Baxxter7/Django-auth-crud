70478bf56f05737b7b0f74cd3088e2c0d2dbdf02
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2023-01-17 11:30:59 -0600

    Guardando tareas en la base de datos desde el frontend

--- tasks/views.py
+++ tasks/views.py
@@ -42,17 +42,25 @@ def signup(request):
 def tasks(request):
     return render(request, 'tasks.html')
 
+
 def create_task(request):
     if request.method == "GET":
         return render(request, 'create_task.html', {
             'form': TaskForm
         })
     else:
-        print(request.POST)
-        return render(request, 'create_task.html', {
-            'form': TaskForm
-        })
-        #create_task = Task()
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except:
+            return render(request, 'create_task.html', {
+            'form': TaskForm,
+            'error': 'Please, provide valida data'
+            })
+            # create_task = Task()
 
 
 def signout(request):
